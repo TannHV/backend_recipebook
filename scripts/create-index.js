59024a2cf9bbd0f5daff48946b279ffda6d712a0
TannHV <dtam3002@gmail.com>
2025-08-26 00:25:39 +0700

    update backend source code

---
+++ scripts/create-index.js
@@ -0,0 +1,23 @@
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
+
+(async () => {
+    const client = new MongoClient(process.env.MONGO_URI);
+    await client.connect();
+    const db = client.db();
+
+    await db.collection('users').createIndex({ email: 1 }, { unique: true });
+    await db.collection('users').createIndex({ username: 1 }, { unique: true });
+
+    await db.collection('recipes').createIndex({ createdAt: -1 });
+    await db.collection('recipes').createIndex({ tags: 1 });
+    await db.collection('recipes').createIndex({ "time.total": 1 });
+    await db.collection('recipes').createIndex({ createdBy: 1 });
+    await db.collection('recipes').createIndex({ title: "text", summary: "text", "ingredients.name": "text" });
+
+    await db.collection('blogs').createIndex({ createdAt: -1 });
+    await db.collection('blogs').createIndex({ author: 1 });
+
+    console.log('Index created');
+    await client.close();
+})();
